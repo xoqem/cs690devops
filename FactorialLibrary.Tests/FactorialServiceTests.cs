6e8fd507b9b116e938163b70d998250224a14dce
Jonathan Cornwell <jonathan.m.cornwell@gmail.com>
2025-01-09 05:12:54 +0000

    create FactorialLibrary

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,11 @@
+namespace FactorialService.Tests;
+using FactorialLibrary;
+public class FactorialServiceTests
+{
+  [Fact]
+  public void Test1()
+  {
+    var result = FactorialService.Factorial(0);
+    Assert.Equal(1,result);
+  }
+}
