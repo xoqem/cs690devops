b328d39220da13657f2c44056b8e35c44bb8e614
Jonathan Cornwell <jonathan.m.cornwell@gmail.com>
2025-01-09 05:29:11 +0000

    Negative feature

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -8,4 +8,12 @@ public class FactorialServiceTests
     var result = FactorialService.Factorial(0);
     Assert.Equal(1,result);
   }
+
+  [Fact]
+  public void Test_negative()
+  {
+    Assert.Throws<InvalidOperationException>(()=>{
+      var result = FactorialService.Factorial(-1);
+    });
+  }
 }
